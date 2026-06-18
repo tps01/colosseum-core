9b369da926fab4ae100e719a9bd19d63fe2c1c31
tps01 <thomaspatricksullivan@gmail.com>
2026-06-18 16:22:52 -0400

    0.12.2

--- colosseum/__init__.py
+++ colosseum/__init__.py
@@ -12,7 +12,7 @@ from .decorators import (
 from .plugins.namespace import LazyNamespaceProxy
 from .results import endex
 
-__version__ = "0.12.1"
+__version__ = "0.12.2"
 
 equipment = LazyNamespaceProxy("equipment")
 shared = LazyNamespaceProxy("shared")
